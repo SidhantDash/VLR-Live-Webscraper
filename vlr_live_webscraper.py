9459c3f2d2d97417c5b037364da344c4778f4b2f
SidhantDash <sidhantdash126@gmail.com>
2025-09-15 23:56:17 -0400

    Add intial files

---
+++ vlr_live_webscraper.py
@@ -0,0 +1,26 @@
+import requests
+from lxml import html
+
+match_url = 'https://www.vlr.gg/matches'
+
+response = requests.get(match_url)
+tree = html.fromstring(response.content)
+
+
+
+# Extract 10 upcoming matches
+# matches = tree.xpath('//a[contains(@class, "match-item")]/@href')
+# matchesnode = tree.xpath('//a[contains(@class, "match-item")]//div/node()')
+
+# for match in matches:
+#     print(match.strip())
+
+
+# Checks if there is a live match going on
+def is_live_match():
+    liveElement = tree.xpath('//div[contains(@class, "ml-status") and contains(text(), "LIVE")]')
+    parentLiveElement = tree.xpath('..//div[contains(@class, "ml-status") and contains(text(), "LIVE")]')
+    for element in liveElement:
+        print(element.text_content().strip())
+
+is_live_match()
